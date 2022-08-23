7fb20f24584329a2f06f56254f056d1d050b99f8
Emmanuel <emmanue.kimweli@student.moringaschool.com>
2022-08-23 13:36:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+let name = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(name,event){
+    let msg = [] ;
+    for(let n=0;n<name.length;n++){
+     msg.push(`Thank you, ${name[n]}, for the wonderful ${event} gift!`);
+}
+return msg;
+}
+
+
+
+function countDown(num){
+    while (num>=0){
+        console.log(num);
+        num--;
+    }
+}
